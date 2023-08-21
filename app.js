0ed6d6bb974937861216d810d35b809180e5b481
Pius Agyekum <agyekumpius1@gmail.com>
2023-08-21 13:44:54 +0000

    controllers

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+const {
+  all_blogs,
+  find_blog,
+  delete_blog,
+  add_blog,
+  edit_blog,
+} = require("./controllers/blogController")
+
+const app = express()
+app.use(morgan("dev"))
+app.use(express.json())
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("listening for requests on port", process.env.PORT)
+    })
+  })
+  .catch(() => {
+    console.log("Could not connect to the database")
+  })
+
+app.get("/blogs", all_blogs)
+
+app.get("/blogs/:id", find_blog)
+
+app.delete("/blogs/:id", delete_blog)
+
+app.post("/blogs/add", add_blog)
+
+app.patch("/blogs/:id", edit_blog)
---
+++ controllers/blogController.js
@@ -0,0 +1,71 @@
+const Blog = require("../models/blog")
+const mongoose = require("mongoose")
+
+const all_blogs = (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then(blogs => {
+      res.json({ code: 0, blogs })
+    })
+    .catch(err => {
+      res.json({ code: 1, Message: "Could not retrive blogs" })
+    })
+}
+
+const find_blog = (req, res) => {
+  Blog.findById(req.params.id)
+    .then(blog => {
+      res.json({ code: 0, blog })
+    })
+    .catch(err => {
+      res.json({ code: 1, Message: "Blog was not found" })
+    })
+}
+
+const delete_blog = (req, res) => {
+  const { id } = req.params
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ code: 1, Message: "An invalid Id was provided" })
+  }
+
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ code: 0, Message: "The blog was deleted successfully" })
+    })
+    .catch(err => {
+      res.json({ code: 1, Message: "Blog was not found" })
+    })
+}
+
+const add_blog = (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog
+    .save()
+    .then(result => {
+      res.json({ code: 0, Message: "Blog was added successfully", result })
+    })
+    .catch(err => {
+      res.json({ code: 1, Message: "Blog was not added" })
+    })
+}
+
+const edit_blog = (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.json({ code: 0, Message: "Blog was updated successfully" })
+    })
+    .catch(err => {
+      res.json({ code: 1, Message: "Blog was not found" })
+    })
+}
+
+module.exports = {
+  all_blogs,
+  find_blog,
+  delete_blog,
+  add_blog,
+  edit_blog,
+}
