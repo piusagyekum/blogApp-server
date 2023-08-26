a8ea82c4b5cbb6d40026303eae9918d0c45ec137
Pius Agyekum <agyekumpius1@gmail.com>
2023-08-26 11:18:16 +0000

    sign up pattern modified

--- util/signupValidation.js
+++ util/signupValidation.js
@@ -2,8 +2,11 @@ const Joi = require("joi")
 
 const signupSchema = Joi.object().keys({
   email: Joi.string().email({ tlds: { allow: false } }),
-  password: Joi.string()
-    .pattern(/"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"/)
+  password: Joi.string().min(6)
+    .pattern(/[a-z]+/)
+    .pattern(/[A-Z]+/)
+    .pattern(/[\d]+/)
+    .pattern(/[^\w\d]+/)
     .messages({
       "string.pattern.base":
         "password is not strong enough",
